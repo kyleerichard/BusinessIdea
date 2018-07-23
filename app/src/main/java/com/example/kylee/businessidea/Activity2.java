773ac1e44dc204786baf5869b162fff74d6c81a8
Kylee R <kylee.richard12@gmail.com>
2018-07-23 13:07:03 -0500

    Initial commit

---
+++ app/src/main/java/com/example/kylee/businessidea/Activity2.java
@@ -0,0 +1,22 @@
+package com.example.kylee.businessidea;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+
+        import android.support.v7.app.AppCompatActivity;
+        import android.os.Bundle;
+
+public class Activity2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main2);
+    }
+}
+
---
+++ app/src/main/java/com/example/kylee/businessidea/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.kylee.businessidea;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.content.Intent;
+
+public class MainActivity extends AppCompatActivity {
+    private Button button;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = (Button) findViewById(R.id.whybestsellerbutton);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity2();
+            }
+        });
+    }
+    public void openActivity2(){
+        startActivity((new Intent(MainActivity.this, Activity2.class)));
+    }
+
+
+    public void openActivity2(View view) {
+    }
+}
